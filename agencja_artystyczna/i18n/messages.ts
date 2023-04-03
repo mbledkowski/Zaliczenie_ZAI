03beb0cfa0e2dcf372698f31cb6efc3be4326252
mbledkowski <pub@mble.dk>
2023-04-03 19:08:19 +0200

    Add additional strings to i18n

--- agencja_artystyczna/i18n/messages.ts
+++ agencja_artystyczna/i18n/messages.ts
@@ -1,10 +1,23 @@
 export default {
   en: {
     welcome: 'Welcome',
-    comName: 'Artistic Agency'
+    comName: 'Artistic Agency',
+    // "About Us", "Organization", "Technique", "Service", "Events", and "Contact".
+    about: 'About Us',
+    organization: 'Organization',
+    technique: 'Technique',
+    service: 'Service',
+    events: 'Events',
+    contact: 'Contact',
   },
   pl: {
     welcome: 'Witaj',
-    comName: 'Agencja Artystyczna'
+    comName: 'Agencja Artystyczna',
+    about: 'O nas',
+    organization: 'Organizacja',
+    technique: 'Technika',
+    service: 'Usługi',
+    events: 'Wydarzenia',
+    contact: 'Kontakt',
   }
 }
