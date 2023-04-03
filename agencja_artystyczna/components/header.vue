b92614e2c0f22636290aacd8e93b36bdfda549bc
mbledkowski <pub@mble.dk>
2023-04-03 19:03:17 +0200

    Update header

--- agencja_artystyczna/components/header.vue
+++ agencja_artystyczna/components/header.vue
@@ -5,8 +5,26 @@
         <div class="navbar-start">
           <div class="dropdown">
             <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a></a></li>
-              <li><a></a></li>
+              <ul class="menu menu-horizontal px-1">
+                <li>
+                  <NuxtLink to="/about">{{ $t('about') }}</NuxtLink>
+                </li>
+                <li></li>
+                <NuxtLink to="/organization">{{ $t('organization') }}</NuxtLink>
+                </li>
+                <li></li>
+                <NuxtLink to="/technique">{{ $t('technique') }}</NuxtLink>
+                </li>
+                <li></li>
+                <NuxtLink to="/service">{{ $t('service') }}</NuxtLink>
+                </li>
+                <li></li>
+                <NuxtLink to="/events">{{ $t('events') }}</NuxtLink>
+                </li>
+                <li></li>
+                <NuxtLink to="/contact">{{ $t('contact') }}</NuxtLink>
+                </li>
+              </ul>
             </ul>
           </div>
           <NuxtLink class="btn btn-ghost normal-case text-xl" to="/">{{ $t('comName') }}</NuxtLink>
