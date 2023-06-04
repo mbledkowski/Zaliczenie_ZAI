de3bc93ff9e3c22c5ee9bc0b1a345ec3a6459465
ala <pawelx.sz@gmail.com>
2023-06-04 16:34:42 +0200

    about us

--- agencja_artystyczna/pages/about.vue
+++ agencja_artystyczna/pages/about.vue
@@ -1,11 +1,17 @@
 <template>
-  <header class="h-1">
+  <div class="aboutUs">
     <h1 class="title">About us</h1>
-  </header>
+  </div>
 </template>
 
 <style lang="css" scoped>
-  .h-1 {
+  .aboutUs {
+    background-color: #20242e;
+    border-radius: 0.5rem;
+    margin-left: 15px;
+    margin-right: 15px;
+    margin-top: 90px;
+    font-size: 25px;
     position: relative;
     top: 50%;
     text-align: center;
