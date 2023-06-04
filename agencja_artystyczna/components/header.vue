b5c64dc2b591e347731d593bf0a3a4e80160fe9e
ala <pawelx.sz@gmail.com>
2023-06-04 19:17:55 +0200

    header

--- agencja_artystyczna/components/header.vue
+++ agencja_artystyczna/components/header.vue
@@ -1,57 +1,65 @@
 <template>
   <header class="h-0">
     <div class="sticky left-0 top-0 p-4 flex">
-      <div class="navbar bg-base-100 rounded-lg">
-        <div class="navbar-start">
-          <div class="dropdown">
-            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <ul class="menu menu-horizontal px-1">
-                <li>
-                  <NuxtLink to="/about">{{ $t('about') }}</NuxtLink>
-                </li>
-                <li>
-                  <NuxtLink to="/organization">{{ $t('organization') }}</NuxtLink>
-                </li>
-                <li>
-                  <NuxtLink to="/technique">{{ $t('technique') }}</NuxtLink>
-                </li>
-                <li>
-                  <NuxtLink to="/service">{{ $t('service') }}</NuxtLink>
-                </li>
-                <li>
-                  <NuxtLink to="/events">{{ $t('events') }}</NuxtLink>
-                </li>
-                <li>
-                  <NuxtLink to="/contact">{{ $t('contact') }}</NuxtLink>
-                </li>
+      <div class="navbar">
+        <div class="navbar bg-base-100 rounded-lg">
+          <div class="navbar-start">
+            <div class="dropdown">
+              <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <ul class="menu menu-horizontal px-1">
+                  <li>
+                    <NuxtLink to="/about">{{ $t('about') }}</NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="/organization">{{ $t('organization') }}</NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="/technique">{{ $t('technique') }}</NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="/service">{{ $t('service') }}</NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="/events">{{ $t('events') }}</NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="/contact">{{ $t('contact') }}</NuxtLink>
+                  </li>
+                </ul>
               </ul>
+            </div>
+            <NuxtLink class="btn btn-ghost normal-case text-xl" to="/">{{ $t('comName') }}</NuxtLink>
+          </div>
+          <div class="navbar-end hidden lg:flex">
+            <ul class="menu menu-horizontal px-1">
+              <li>
+                <NuxtLink to="/about">{{ $t('about') }}</NuxtLink>
+              </li>
+              <li>
+                <NuxtLink to="/organization">{{ $t('organization') }}</NuxtLink>
+              </li>
+              <li>
+                <NuxtLink to="/technique">{{ $t('technique') }}</NuxtLink>
+              </li>
+              <li>
+                <NuxtLink to="/service">{{ $t('service') }}</NuxtLink>
+              </li>
+              <li>
+                <NuxtLink to="/events">{{ $t('events') }}</NuxtLink>
+              </li>
+              <li>
+                <NuxtLink to="/contact">{{ $t('contact') }}</NuxtLink>
+              </li>
             </ul>
           </div>
-          <NuxtLink class="btn btn-ghost normal-case text-xl" to="/">{{ $t('comName') }}</NuxtLink>
-        </div>
-        <div class="navbar-end hidden lg:flex">
-          <ul class="menu menu-horizontal px-1">
-            <li>
-              <NuxtLink to="/about">{{ $t('about') }}</NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/organization">{{ $t('organization') }}</NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/technique">{{ $t('technique') }}</NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/service">{{ $t('service') }}</NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/events">{{ $t('events') }}</NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/contact">{{ $t('contact') }}</NuxtLink>
-            </li>
-          </ul>
         </div>
       </div>
     </div>
   </header>
 </template>
+
+<style lang="css" scoped>
+.navbar {
+  margin-top: 14px;
+}
+</style>
--- agencja_artystyczna/pages/index.vue
+++ agencja_artystyczna/pages/index.vue
@@ -1,3 +1,16 @@
 <template>
+  <div class="motto">
+    <h1 class="title1">Taniec, Śpiew, Malarstwo, Organizacja</h1>
+  </div>
   <hero />
 </template>
+
+<style lang="css" scoped>
+.title1 {
+  padding-top: 3px;
+  background-color: #20242e;
+  font-size: 30px;
+  position: relative;
+  text-align: center;
+}
+</style>
