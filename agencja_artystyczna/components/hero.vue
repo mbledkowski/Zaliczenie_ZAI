5a455a5d78681a18a3d5e113df034800bb62ec86
mbledkowski <pub@mble.dk>
2023-06-06 20:49:46 +0200

    Small change to hero

--- agencja_artystyczna/components/hero.vue
+++ agencja_artystyczna/components/hero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hero min-h-screen bg">
     <div class="hero-overlay bg-opacity-60"></div>
-    <div class="hero-content flex flex-col flex-nowrap text-center text-neutral-content p-6.875">
+    <div class="hero-content flex flex-col flex-nowrap text-center text-neutral-content mt-20">
       <slider class="max-w-fit rounded-lg shadow-2xl"/>
       <div class="max-w-md">
         <h1 class="mb-5 text-5xl font-bold">{{ $t('comName') }}</h1>
