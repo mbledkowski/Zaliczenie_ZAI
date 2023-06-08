e2dc825c7cb4f77a3e5e9c2936ba4dff76dcc13d
mbledkowski <pub@mble.dk>
2023-06-08 17:08:35 +0200

    Add 10 images to home page

--- agencja_artystyczna/pages/index.vue
+++ agencja_artystyczna/pages/index.vue
@@ -1,6 +1,38 @@
 <template>
   <hero />
+  <section class="flex flex-row flex-wrap">
+    <div v-for="(image, index) in firstSecImages" :key="index" class="w-full md:w-1/4 aspect-[20/12]">
+      <img :src="image" :alt="image" class="w-full h-full object-cover" />
+    </div>
+  </section>
+  <section class="flex flex-row flex-wrap">
+    <div v-for="(image, index) in secondSecImages" :key="index" class="w-full md:w-1/2 aspect-[20/12]">
+      <img :src="image" :alt="image" class="w-full h-full object-cover" />
+    </div>
+  </section>
 </template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      firstSecImages: [] as string[],
+      secondSecImages: [] as string[],
+    };
+  },
+  methods: {
+    async loadImages() {
+      const response = await fetch("/img/main/img.json");
+      let images = await response.json() as string[];
+      this.firstSecImages = images.slice(0, 8);
+      this.secondSecImages = images.slice(8, 10);
+    },
+  },
+  mounted() {
+    this.loadImages();
+  },
+};
+</script>
 
 <style lang="css" scoped>
 </style>
