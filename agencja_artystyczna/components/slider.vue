3851e04db0f494f2a93bb78aa5f7caa96af7779b
mbledkowski <pub@mble.dk>
2023-06-08 21:02:03 +0200

    Fix slider styles

--- agencja_artystyczna/components/slider.vue
+++ agencja_artystyczna/components/slider.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="carousel w-full" id="slider">
     <div v-for="(image, index) in images" :key="index" class="carousel-item relative w-full">
-      <div :id="'slide' + index" >
+      <div :id="'slide' + index" class="w-full">
         <img :src="image" class="w-full aspect-square md:aspect-[3/1] lg:aspect-[4/1] object-cover" :alt="image"/>
         <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a :href="'#slide' + (index - 1)" v-if="index !== 0" class="btn btn-circle">&#10094;</a>
