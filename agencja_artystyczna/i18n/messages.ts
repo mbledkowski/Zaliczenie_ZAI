a4b40d22d98312d052b54441ecc0c22db9348278
mbledkowski <pub@mble.dk>
2023-06-08 16:01:44 +0200

    Change home name in header

--- agencja_artystyczna/i18n/messages.ts
+++ agencja_artystyczna/i18n/messages.ts
@@ -1,6 +1,7 @@
 export default {
   en: {
     welcome: 'Welcome',
+    shortComName: 'Artistic Agency',
     comName: 'Artistic Agency - Dance, Sing, Paint',
     desc: 'Welcome to the world full of creativity, passion and unforgettable experiences! We are an Artistic Agency, your guide to the fascinating world of art and entertainment. Our mission is to discover, promote and support unique talents that will delight every culture lover.',
     about: 'About Us',
@@ -12,6 +13,7 @@ export default {
   },
   pl: {
     welcome: 'Witaj',
+    shortComName: 'Agencja Artystyczna',
     comName: 'Agencja Artystyczna - Taniec, Śpiew, Malarstwo',
     desc: 'Witaj w świecie pełnym kreatywności, pasji i niezapomnianych przeżyć! Jesteśmy Agencją Artystyczną, twoim przewodnikiem po fascynującym świecie sztuki i rozrywki. Nasza misja to odkrywanie, promowanie i wspieranie wyjątkowych talentów, które zachwycą każdego miłośnika kultury.',
     about: 'O nas',
