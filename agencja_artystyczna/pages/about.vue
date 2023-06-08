d34b088a64bcda603efc8c6063cef17373ecea02
ala <pawelx.sz@gmail.com>
2023-06-08 16:35:49 +0200

    about

--- agencja_artystyczna/pages/about.vue
+++ agencja_artystyczna/pages/about.vue
@@ -1,7 +1,59 @@
 <template>
   <div class="hero min-h-screen" style="background-image: url(/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
-    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-overlay bg-opacity-60">
+      <div class="containerA">
+        <div class="aboutText"><p id="firstP">Witamy na stronie internetowej naszej agencji artystycznej!</p>
+
+          <p>Jesteśmy dynamiczną i kreatywną agencją, która zajmuje się reprezentacją artystów z różnych dziedzin. Nasza
+          misja polega na odkrywaniu, wspieraniu i promowaniu utalentowanych artystów, aby pomóc im osiągnąć sukces i
+          zrealizować swoje marzenia.</p>
+
+          <p>Łączymy pasję z profesjonalizmem, aby zapewnić naszym klientom wysokiej
+          jakości usługi. Nasz zespół składa się z doświadczonych agentów, menedżerów artystycznych i ekspertów w
+          dziedzinie marketingu, którzy są w pełni zaangażowani w rozwój kariery naszych artystów.</p>
+
+          <p>Wierzymy w indywidualne podejście do każdego artysty, który dołącza do naszej agencji. Pragniemy zrozumieć ich
+          wizję, cele i unikalne talenty, aby móc skutecznie współpracować przy budowaniu ich marki i kreowaniu
+          długotrwałej kariery. Dążymy do tego, aby nasi artyści mieli szerokie możliwości rozwoju i byli obecni zarówno
+          na lokalnych, jak i międzynarodowych scenach artystycznych.</p>
+
+          <p>Nasza agencja reprezentuje artystów z różnych dziedzin, w tym muzyków, tancerzy, malarzy i
+          wielu innych. Niezależnie od tego, czy jesteś doświadczonym artystą, czy dopiero rozpoczynasz swoją karierę,
+          chętnie przyjrzymy się Twojemu portfolio i rozważymy możliwość współpracy.</p>
+
+          <p>Współpracując z nami, możesz oczekiwać profesjonalnego podejścia, wsparcia w budowaniu swojej marki, kreacji
+          strategii rozwoju kariery oraz organizacji występów i projektów artystycznych. Dbamy o naszych artystów na
+          każdym etapie ich kariery i staramy się zapewnić im jak najlepsze warunki do rozwijania swojego talentu i
+          osiągania sukcesu.</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>.containerA {
+  position: absolute;
+  top: 55%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 70%;
+  width: 80%;
+  background-color: #2a303c;
+  border-radius: 1.5em;
+}
+
+.aboutText {
+  margin: 20px;
+  font-size: 18px;
+
+}
+
+p {
+  margin-top: 12px;
+}
+
+#firstP{
+  font-size: 25px;
+  margin-bottom: 40px;
+}
+</style>
